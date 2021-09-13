5d5b6a903736d6530dfb747bd55bc1c692288dd8
aleksey <atonishev@phoenixit.ru>
2021-09-13 19:53:09 +0300

    удалил изменения в миграциях

--- db/migrate/20210910181313_create_tests.rb
+++ db/migrate/20210910181313_create_tests.rb
@@ -2,9 +2,9 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.string :body, null: false
       t.integer :level, default: 0
       t.references :category, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
