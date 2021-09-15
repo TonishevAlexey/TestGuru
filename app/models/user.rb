bea52c29dd7fe9068816340dc78852a6361c0bfe
aleksey <atonishev@phoenixit.ru>
2021-09-15 20:40:39 +0300

    1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :user_tests
   has_many :test, through: :user_tests
   def test_level(level)
-    test.where("level = ?", level)
+    test.where(level: level)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "Alex")
+user = User.create(name: "Alex")
 User.create(name: "Oleg")
 
-Category.create(title: "Ruby1")
-category = Category.first
-user = User.first
-Test.create(title: "Rails", category_id: category.id)
-test = Test.first
-Question.create(title: "Rails model")
-answer = Answer.first
+category = Category.create(title: "Ruby1")
+test = Test.create(title: "Rails", category_id: category.id)
+
+answer = Question.create(title: "Rails model")
 Answer.create(body: "123", question_id: answer.id)
 UserTest.create(user_id: user, test_id: test)
