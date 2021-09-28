560d2910733b33cf00dac043eccb1407c8e5ae2a
aleksey <atonishev@phoenixit.ru>
2021-09-28 12:45:37 +0300

    исправил недочеты

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :tests_category, -> (category) { joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title) }
+  scope :tests_category, -> (category) { where(categories: { title: category }) }
 
   belongs_to :category
   has_many :user_tests
@@ -11,7 +11,9 @@ class Test < ApplicationRecord
 
   validates :body, :title, presence: true
   validates :level, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, presence: true, uniqueness: { scope: :level }
 
-  validates_uniqueness_of :title, scope: :level
-
+  def self.tests_category(category)
+    joins(:category).tests_category(category).order(title: :desc).pluck(:title)
+  end
 end
