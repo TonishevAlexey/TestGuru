352f0d536be338ea97a3a2de6e5cd6c6ad5c76dc
aleksey <atonishev@phoenixit.ru>
2021-09-29 14:15:41 +0300

    дз 8

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,51 @@
+class TestsController < ActionController::Base
+  before_action :find_test, only: [:show, :edit, :update, :destroy]
+
+  def index
+    render plain: Test.all.pluck(:title, :body, :level)
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def show
+    render plain: @test.title
+  end
+
+  def edit
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+end
