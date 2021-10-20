9da05ff09648d9a3520a1971302415600ecb14a9
aleksey <atonishev@phoenixit.ru>
2021-10-20 20:14:25 +0300

    default_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, alert: t('.message') if current_user.admin?
   end
 
+  def default_url_options
+    I18n.locale != I18n.default_locale ? { lang: I18n.locale } : {}
+  end
+
   def set_locale
     I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
