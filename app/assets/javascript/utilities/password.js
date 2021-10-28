b921abd4f18b2ef8c34b1e4b655a2898fd26dcb4
aleksey <atonishev@phoenixit.ru>
2021-10-28 12:57:40 +0300

    update password

--- app/assets/javascript/utilities/password.js
+++ app/assets/javascript/utilities/password.js
@@ -1,16 +1,13 @@
 document.addEventListener('turbolinks:load', function () {
-    const buttonElement = document.querySelectorAll("#user_password_confirmation, #user_password");
+    const buttonElement = document.getElementById('user_password_confirmation');
     const confirm = document.getElementById('confirmation');
-    buttonElement.forEach(function(item) {
-        item.addEventListener('input', function (event) {
+    buttonElement.addEventListener('input', function (event) {
             var password = document.getElementById('user_password').value;
             var confirm_password = document.getElementById('user_password_confirmation').value;
-            if (password === confirm_password && confirm_password !== '') {
-                confirm.lastChild.nodeValue = '✅';
-            } else if (confirm_password !== '' && password.length <= confirm_password.length) {
+            if (confirm_password !== '' && password.length <= confirm_password.length) {
                 confirm.lastChild.nodeValue = '⛔'
+                if (password === confirm_password && confirm_password !== '') {confirm.lastChild.nodeValue = '✅';}
             }
-        });
 
     });
 })
