5aa960b9da0f957c5c78512bf95efd9457c5e791
aleksey <atonishev@phoenixit.ru>
2021-10-29 00:22:06 +0300

    add password and sorting

--- app/assets/javascript/utilities/password.js
+++ app/assets/javascript/utilities/password.js
@@ -4,9 +4,9 @@ document.addEventListener('turbolinks:load', function () {
     buttonElement.addEventListener('input', function (event) {
             var password = document.getElementById('user_password').value;
             var confirm_password = document.getElementById('user_password_confirmation').value;
-            if (confirm_password !== '' && password.length <= confirm_password.length) {
+            if (confirm_password !== '') {
                 confirm.lastChild.nodeValue = '⛔'
-                if (password === confirm_password && confirm_password !== '') {confirm.lastChild.nodeValue = '✅';}
+                if (password === confirm_password) {confirm.lastChild.nodeValue = '✅';}
             }
 
     });
