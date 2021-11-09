d702e4ac8a2e9ce9c389ad3e60160753d498e7c3
aleksey <atonishev@phoenixit.ru>
2021-11-09 16:45:33 +0300

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
   def contact_email
     @contact_message = params[:contact_message]
-    mail(to: ENV['ADMIN_EMAIL'], from: ENV['ADMIN_EMAIL'], subject: @contact_message.text)
+    mail(to: ENV['ADMIN_EMAIL'], from: @contact_message.email, subject: @contact_message.text)
   end
 end
