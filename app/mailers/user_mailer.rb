918cf193b7f58e67faacbca54c074a99d0f7fd97
aleksey <atonishev@phoenixit.ru>
2021-11-09 16:19:27 +0300

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,5 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
-  layout 'mailer'
   def contact_email
     @contact_message = params[:contact_message]
     mail(to: ENV['ADMIN_EMAIL'], from: ENV['ADMIN_EMAIL'], subject: @contact_message.text)
--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,5 +1,6 @@
 class ContactMessage < ApplicationRecord
   belongs_to :user
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.(\w|[a-z]){2,3}\z/i
 
   validates :text, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL }
