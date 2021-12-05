5303cc5542fa369cfb0bf9a22cba033fbb986056
aleksey <atonishev@phoenixit.ru>
2021-12-05 21:23:58 +0300

    update

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -18,17 +18,17 @@ class BadgeService
   end
 
   def first_badge?
-    @current_user.test_passages_count == 1 && @test_passage.success == true && Badge.find_by(kind: 'first') != nil
+    @current_user.test_passages_count == 1 && @test_passage.success == true
 
   end
 
   def level_badge?
-    Test.tests_level(@test_passage.test.level).count == @current_user.tests.tests_level(@test_passage.test.level).count && @current_user.tests.tests_level(@test_passage.test.level).count != nil && Badge.find_by(kind: 'level') != nil
+    Test.tests_level(@test_passage.test.level).count == @current_user.tests.tests_level(@test_passage.test.level).count && @current_user.tests.tests_level(@test_passage.test.level).count != nil
 
   end
 
   def category_badge?
-    Test.tests_category(@test_passage.test.category.title).count == @current_user.tests.tests_category(@test_passage.test.category.title).where(test_passages: { success: true }).count && @current_user.tests.tests_category(@test_passage.test.category.title).where(test_passages: { success: true }).count != nil && Badge.find_by(kind: 'category') != nil
+    Test.tests_category(@test_passage.test.category.title).count == @current_user.tests.tests_category(@test_passage.test.category.title).where(test_passages: { success: true }).count && @current_user.tests.tests_category(@test_passage.test.category.title).where(test_passages: { success: true }).count != nil
 
   end
 
